2d5c58aed574f9c8faa3a130df1396c9935d5abb
A-Kija <arvydas.kijakauskas@gmail.com>
2022-01-21 18:52:51 +0200

    for 2

--- 002/i.js
+++ 002/i.js
@@ -1,3 +1,31 @@
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 for (let aukstas = 0; aukstas < 3; aukstas++) {
     console.log('Aukštas:', aukstas);
+}
+
+
+for (let i = 0; i < 3; i++) {
+    console.log('Africa');
+}
+
+
+
+
+
+for (let i = 0; i < 3; i++) {
+
+    console.log(rand(3, 13));
+
+}
+
+
+for (let i = 0; i < 3; i++) {
+
+    console.log(5 + 8);
+
 }
