30bd621288f10cb40d57587534d7580674995f6b
A-Kija <arvydas.kijakauskas@gmail.com>
2022-01-21 19:28:49 +0200

    for 3

--- 002/i.js
+++ 002/i.js
@@ -29,3 +29,41 @@ for (let i = 0; i < 3; i++) {
     console.log(5 + 8);
 
 }
+
+
+
+for (let i = 0; i < 15; i++) {
+
+    let sk1 = rand(5, 15);
+    let sk2 = rand(5, 15);
+    console.log(sk1, 'X', sk2, '=', sk1 * sk2);
+
+}
+
+console.log('------------------------------------------');
+
+for (let i = 0; i < 10; i++) {
+
+    let kauliukas = rand(1, 6);
+
+    if (kauliukas > 3) {
+        console.log(kauliukas);
+    }
+
+
+}
+
+
+console.log('------------------------------------------');
+
+for (let i = 0; i < 10; i++) {
+
+    let kauliukas = rand(1, 6);
+
+    if (kauliukas < 3) {
+        console.log(kauliukas);
+    } else {
+        console.log('Valio!');
+    }
+
+}
