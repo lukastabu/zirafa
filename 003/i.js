7903ae372c7d92789528aa344307e25880d0d07d
A-Kija <arvydas.kijakauskas@gmail.com>
2022-01-22 10:15:50 +0200

    array

--- 003/i.js
+++ 003/i.js
@@ -5,42 +5,61 @@ function rand(min, max) {
 }
 
 
-console.log('------------------------------------------');
+// console.log('------------------------------------------');
 
 
-let kibiras5 = 0;
+// let kibiras5 = 0;
 
 
-for (let i = 0; i < 20; i++) {
+// for (let i = 0; i < 20; i++) {
 
-    if (i == 7 || i == 13) {
-        continue;
-    }
+//     if (i == 7 || i == 13) {
+//         continue;
+//     }
 
-    let siuksliu = rand(5, 105);
-    kibiras5 = kibiras5 + siuksliu;
-}
+//     let siuksliu = rand(5, 105);
+//     kibiras5 = kibiras5 + siuksliu;
+// }
 
-console.log(kibiras5);
-
-
-console.log('------------------------------------------');
+// console.log(kibiras5);
 
 
-let kibiras6 = 0;
+// console.log('------------------------------------------');
 
 
-for (let i = 0; i < 20; i = i + 3) {
-
-    let siuksliu = rand(5, 105);
-    kibiras6 = kibiras6 + siuksliu;
-}
-
-console.log(kibiras6);
+// let kibiras6 = 0;
 
 
-console.log('------------------------------------------');
+// for (let i = 0; i < 20; i = i + 3) {
 
-for (let i = 19; i >= 0; i--) {
-    console.log(i);
+//     let siuksliu = rand(5, 105);
+//     kibiras6 = kibiras6 + siuksliu;
+// }
+
+// console.log(kibiras6);
+
+
+// console.log('------------------------------------------');
+
+// for (let i = 19; i >= 0; i--) {
+//     console.log(i);
+// }
+
+
+let smallHouse = 'Zita';
+
+let bigHouse = ['James', 'Zita', 'Bond'];
+
+let cats = ['Murka', 'Pilkis', 'Keris', 'Ūkas', 'Rainis', 'Pilkis', 'Murka', 'Pilkis'];
+
+
+smallHouse = 'Bebras';
+bigHouse[1] = 'Barsukas';
+
+
+console.log(smallHouse, bigHouse[0], bigHouse[2], bigHouse[1], bigHouse.length);
+
+
+for (let i = 0; i < bigHouse.length; i++) {
+    console.log(bigHouse[i]);
 }
