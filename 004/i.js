e3d61fbe91587cc585161f2c64629a22e0e744a5
A-Kija <arvydas.kijakauskas@gmail.com>
2022-01-24 19:23:11 +0200

    testas

---
+++ 004/i.js
@@ -0,0 +1,56 @@
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+console.log('------------1-------------');
+for (let i = 0; i < 11; i++) {
+    console.log(i * 10 + 100);
+}
+
+console.log('------------2-------------');
+let double = 0;
+for (let i = 0; i < 10; i++) {
+    let random = rand(5, 15);
+    console.log(random);
+    if (random > 9) {
+        double++;
+    }
+}
+console.log('Dviženkliai:', double);
+
+console.log('------------3-------------');
+let sum = 0;
+for (let i = 0; i < 10; i++) {
+    let random = rand(5, 15);
+    console.log(random);
+    sum = sum + random;
+}
+console.log('Vidurkis:', sum / 10);
+
+console.log('------------4-------------');
+let it4 = 0;
+let random4;
+do {
+    it4++;
+    random4 = rand(10, 50);
+} while (random4 != 13 && random4 != 33 && random4 != 43);
+console.log(random4, it4);
+
+
+console.log('------------5-------------');
+
+let times = 0;
+let it5 = 0;
+while (times != 3) {
+    it5++;
+    let random = rand(10, 20);
+    if (random == 17) {
+        console.log(random);
+        times++;
+    }
+}
+
+console.log(times, it5);
