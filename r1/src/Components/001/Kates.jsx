6c52cdc24b200213f5293ad77504fb73f4fa65c9
A-Kija <arvydas.kijakauskas@gmail.com>
2022-05-09 22:13:39 +0300

    rand color

--- r1/src/Components/001/Kates.jsx
+++ r1/src/Components/001/Kates.jsx
@@ -1,7 +1,9 @@
+import randColor from "../../Functions/randColor";
+
 function Kates() {
 
     return (
-        <ul className="katinai">
+        <ul className="katinai" style={{color: randColor() }}>
             <li>Murka</li>
             <li>Pilkis</li>
             <li>Pacukas</li>
