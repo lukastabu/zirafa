69847827ede8429c64f01cbeda9768941d0df300
A-Kija <arvydas.kijakauskas@gmail.com>
2022-05-09 21:54:30 +0300

    style

--- r1/src/Components/001/Meska.jsx
+++ r1/src/Components/001/Meska.jsx
@@ -1,5 +1,11 @@
+import rand from '../../Functions/rand';
+
 function Meska() {
-    return <h3>Aš meška, o kas tu?</h3>
+    return <h3 style={
+        {
+            color: rand(0, 1) ? 'red' : 'blue'
+        }
+    }>Aš meška, o kas tu?</h3>
 }
 
 export default Meska;
