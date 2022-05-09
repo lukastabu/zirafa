7777b8914f10459fb84d388593e10987bd45e631
A-Kija <arvydas.kijakauskas@gmail.com>
2022-05-09 20:46:28 +0300

    rand numeris

--- r1/src/Components/001/Vovere.jsx
+++ r1/src/Components/001/Vovere.jsx
@@ -1,12 +1,14 @@
+import rand from '../../Functions/rand';
+
 function Vovere() {
 
     return (
         <>
             <div>
-                <div className="vovere">Voverė</div>
+                <div className="vovere">Voverė Nr.: { 7 * 8 }</div>
                 <b>Stora Voverė</b>
             </div>
-            <h3>Voverių medis</h3>
+            <h3>Voverių medis Nr.: {rand(100, 999)} </h3>
         </>
     );
 
