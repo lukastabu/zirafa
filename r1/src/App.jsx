67b54dbb31303a85024f845d0db326947ede3efc
A-Kija <arvydas.kijakauskas@gmail.com>
2022-05-10 21:02:49 +0300

    price tag

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
         <header className="App-header">
           <h1>PROPS</h1>
           <Car price={5500} markSize="10" mark="red" maker="Opel" color="blue" size="25" ff="'Courier New', Courier, monospace"></Car>
-          <Car price={6987} markSize="40" mark="yellow" maker="Volvo" color="dark red" size="24" ff="'Times New Roman', Times, serif"></Car>
+          <Car price={3500} markSize="40" mark="yellow" maker="Volvo" color="dark red" size="24" ff="'Times New Roman', Times, serif"></Car>
           <Car price={500} markSize="33" mark="green" maker="MB" color="silver" size="28" ff="Arial, Helvetica, sans-serif"></Car>
         </header>
       </div>
