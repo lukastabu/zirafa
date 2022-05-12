95629076b57bdc871e9c99966ad3f1f6c94bbe04
A-Kija <arvydas.kijakauskas@gmail.com>
2022-05-12 19:55:57 +0300

    Click

--- r1/src/Components/004/FatCat.jsx
+++ r1/src/Components/004/FatCat.jsx
@@ -2,9 +2,24 @@ import React from "react";
 
 class FatCat extends React.Component {
 
+    constructor() {
+        super();
+        this.state = {weight: 5}; //pradinis tiesiai
+        // this.paspaudimas = this.paspaudimas.bind(this); old school
+    }
+
+    paspaudimas = () => {
+        console.log('Ivyko butono paspaudimo procesas');
+        // console.log(this);
+        this.setState({weight: 6}); // keitimas tik per setState
+    }
+
     render() {
         return (
-            <h2>Fat Cat {this.props.color}</h2>
+            <>
+            <h2>Fat Cat {this.props.color} {this.state.weight}kg</h2>
+            <button onClick={this.paspaudimas}>Click Cat</button>
+            </>
         );
     }
     
