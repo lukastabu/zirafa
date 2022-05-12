8942f1b8914707cfca3b0e377de25db389dfb800
A-Kija <arvydas.kijakauskas@gmail.com>
2022-05-12 18:35:55 +0300

    class

---
+++ class/app.js
@@ -0,0 +1,33 @@
+console.log('CLASS');
+
+
+class Cat {
+
+    constructor(catName, age) {
+        this.name = catName;
+        this.age = age;
+    }
+
+    say() {
+        console.log('Miauuuuu!');
+    }
+
+    sayCatName() {
+        console.log(this.name);
+    }
+
+}
+
+
+const cat1 = new Cat('Pilkis', 4);
+const cat2 = new Cat('Pūkis', 7);
+cat2.color = 'white';
+
+
+
+console.log(cat1);
+
+console.log(cat2);
+
+cat1.sayCatName();
+cat2.sayCatName();
