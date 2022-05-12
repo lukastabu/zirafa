366a91be3aaca50c5c0a82f3efc415696f697b7b
A-Kija <arvydas.kijakauskas@gmail.com>
2022-05-12 20:09:34 +0300

    dog

--- r1/src/Components/004/FatDog.jsx
+++ r1/src/Components/004/FatDog.jsx
@@ -2,9 +2,21 @@ import React from "react";
 
 class FatDog extends React.Component {
 
+    constructor() {
+        super();
+        this.state = {font: 20};
+    }
+
+    spust = () => {
+        this.setState({font: 40})
+    }
+
     render() {
         return (
-            <h3 style={{color: this.props.color}}>Fat Dog</h3>
+            <>
+            <h3 style={{color: this.props.color, fontSize: this.state.font + 'px'}}>Fat Dog</h3>
+            <button onClick={this.spust}>GO</button>
+            </>
         );
     }
     
