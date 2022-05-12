eefd807bd317fb84acecd4d1580a18a2f9512f86
A-Kija <arvydas.kijakauskas@gmail.com>
2022-05-12 20:19:09 +0300

    edit

--- r1/src/Components/004/FatCat.jsx
+++ r1/src/Components/004/FatCat.jsx
@@ -4,21 +4,29 @@ class FatCat extends React.Component {
 
     constructor() {
         super();
-        this.state = {weight: 5}; //pradinis tiesiai
+        this.state = {
+            weight: 5,
+            color: 'blue'
+        }; //pradinis tiesiai
         // this.paspaudimas = this.paspaudimas.bind(this); old school
     }
 
     paspaudimas = () => {
         console.log('Ivyko butono paspaudimo procesas');
         // console.log(this);
-        this.setState({weight: 6}); // keitimas tik per setState
+        this.setState(s => ({weight: s.weight + 1})); // keitimas tik per setState
+    }
+
+    makeRed = () => {
+        this.setState({color: 'red'});
     }
 
     render() {
         return (
             <>
-            <h2>Fat Cat {this.props.color} {this.state.weight}kg</h2>
+            <h2 style={{color: this.state.color}}>Fat Cat {this.props.color} {this.state.weight}kg</h2>
             <button onClick={this.paspaudimas}>Click Cat</button>
+            <button onClick={this.makeRed}>Click RED Cat</button>
             </>
         );
     }
