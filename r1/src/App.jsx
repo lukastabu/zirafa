1e41e2a72101ce62f81e82d1ef654a100e3c8fbd
A-Kija <arvydas.kijakauskas@gmail.com>
2022-05-13 19:56:19 +0300

    rem add

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -6,6 +6,7 @@ function App() {
     const [show, setShow] = useState(true);
     const [show1, setShow1] = useState(true);
     const [color, setColor] = useState('blue');
+    const [one, setOne] = useState(1);
 
     const doShow = () => {
         setShow(s => !s);
@@ -23,6 +24,14 @@ function App() {
         setColor('yellow');
     }
 
+    const addOne = () => {
+        setOne(o => o + 1);
+    }
+
+    const remOne = () => {
+        setOne(o => o - 1);
+    }
+
     return (
         <div className="App">
             <header className="App-header">
@@ -32,11 +41,13 @@ function App() {
                                 {
                     show1 ? <div className="blue-square" style={{background: color}}></div> : null
                 }
-                <h1>HOOK</h1>
-                <button onClick={doShow}>Show/Hide</button>
-                <button onClick={doShow1}>Show/Hide</button>
+                <h1>HOOK {one}</h1>
+                <button onClick={doShow}>Show/Hide 1</button>
+                <button onClick={doShow1}>Show/Hide 2</button>
                 <button onClick={makeRed}>Make Red</button>
                 <button onClick={makeYellow}>Make Yellow</button>
+                <button onClick={addOne}>Add One</button>
+                <button onClick={remOne}>Remove One</button>
             </header>
         </div>
     );
