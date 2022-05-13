d10752c91769c2d910500fd5e743509022f87eb7
A-Kija <arvydas.kijakauskas@gmail.com>
2022-05-13 21:17:09 +0300

    squares

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -7,34 +7,47 @@ function App() {
     const [show1, setShow1] = useState(true);
     const [color, setColor] = useState('blue');
     const [one, setOne] = useState(1);
+    const [sq, setSq] = useState([]);
 
     const makeShow = what => {
         what ? setShow(s => !s) : setShow1(s => !s);
     }
 
     const doMath = number => setOne(o => o + number);
-    
+
 
     const changeColor = color => setColor(color);
+
+    const addSquare = () => setSq(s => [...s, 1]);
 
 
     return (
         <div className="App">
             <header className="App-header">
-                {
-                    show ? <div className="blue-square" style={{background: color}}>1</div> : null
-                }
-                                {
-                    show1 ? <div className="blue-square" style={{background: color}}>2</div> : null
-                }
+                <div className="sqc">
+                    {
+                        sq.map((_, i) => <div className="sq red" key={i}></div>)
+                    }
+                </div>
+                <div className="sqc">
+                    {
+                        show ? <div className="sq" style={{ background: color }}>1</div> : null
+                    }
+                    {
+                        show1 ? <div className="sq" style={{ background: color }}>2</div> : null
+                    }
+                </div>
                 <h1>HOOK {one}</h1>
-                <button onClick={() => makeShow(1)}>Show/Hide 1</button>
-                <button onClick={() => makeShow(0)}>Show/Hide 2</button>
-                <button onClick={() => changeColor('red')}>Make Red</button>
-                <button onClick={() => changeColor('yellow')}>Make Yellow</button>
-                <button onClick={() => changeColor('pink')}>Make Pink</button>
-                <button onClick={() => doMath(1)}>Add One</button>
-                <button onClick={() => doMath(-1)}>Remove One</button>
+                <div className="sqc">
+                    <button onClick={() => makeShow(1)}>Show/Hide 1</button>
+                    <button onClick={() => makeShow(0)}>Show/Hide 2</button>
+                    <button onClick={() => changeColor('red')}>Make Red</button>
+                    <button onClick={() => changeColor('yellow')}>Make Yellow</button>
+                    <button onClick={() => changeColor('pink')}>Make Pink</button>
+                    <button onClick={() => doMath(1)}>Add One</button>
+                    <button onClick={() => doMath(-1)}>Remove One</button>
+                    <button onClick={addSquare}>Add Square</button>
+                </div>
             </header>
         </div>
     );
