1fc0e7c798b43b288c94e6d03ba200d02aec01cf
audronemotiejunaite <89936835+audronemotiejunaite@users.noreply.github.com>
2022-05-17 19:15:38 +0300

    pushh

--- r1/src/Components/james_bond/BlueSquare.jsx
+++ r1/src/Components/james_bond/BlueSquare.jsx
@@ -4,6 +4,7 @@ import randColor from '../../Functions/randColor';
 function BlueSquare() {
 
     const [bg, setBg] = useState('blue');
+    const [border, setBorder] = useState(1);
 
     const changeBg = () => {
         setBg(c => c === 'blue' ? 'red' : 'blue');
@@ -13,10 +14,20 @@ function BlueSquare() {
         setBg(randColor());
     }
 
+    const changeBorder = () => {
+        setBorder(b => b === 10 ? 1 : 10);
+    }
+
     return (
-        <div className="kvadratas" style={{backgroundColor: bg}}>
+        <div className="kvadratas" style={
+            {
+                backgroundColor: bg,
+                borderWidth: border + 'px'
+            }
+            }>
             <button onClick={changeBg}>Change</button>
             <button onClick={changeRand}>Change rand</button>
+            <button onClick={changeBorder}>Change border</button>
         </div>
     )
 }
