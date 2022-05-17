6936fe2e9539b13a7552f22a3bc2e6b75d628486
audronemotiejunaite <89936835+audronemotiejunaite@users.noreply.github.com>
2022-05-17 21:42:56 +0300

    ausys color

--- r1/src/Components/james_bond/Small.jsx
+++ r1/src/Components/james_bond/Small.jsx
@@ -1,14 +1,20 @@
+import { useState } from "react";
+import randColor from "../../Functions/randColor";
+
 function Small({side, sc}) {
+
+    const [color, setColor] = useState(null);
 
     if (!sc) {
         return null;
     }
 
     return (
-        <div className="too-small clickable" style={
+        <div className="too-small clickable" onClick={() => setColor(randColor())} style={
             {
                 left: side === 'left' ? '-10px' : null,
                 right: side === 'right' ? '-10px' : null,
+                backgroundColor: color
             }
         }></div>
     )
