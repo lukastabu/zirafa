f05e2d5f84bf076f85159acea0c7f38d9680f9c3
audronemotiejunaite <89936835+audronemotiejunaite@users.noreply.github.com>
2022-05-18 18:54:33 +0300

    border

---
+++ r1/src/Components/james_bond/ButtonHolder.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+function ButtonHolder({virvute, border}) {
+
+
+
+    return (
+        <div className="bth" style={{borderWidth: border + 'px'}}>
+        <button onClick={virvute}>Change border</button>
+        </div>
+    )
+
+}
+
+export default ButtonHolder;
